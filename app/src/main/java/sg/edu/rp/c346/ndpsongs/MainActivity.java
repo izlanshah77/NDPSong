f9c54d62ae676f2791aaebc2a55be272c48e261e
izlanshah77 <izzy@2000>
2019-05-16 13:36:17 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/ndpsongs/MainActivity.java
@@ -0,0 +1,16 @@
+package sg.edu.rp.c346.ndpsongs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DBHelper dbh = new DBHelper(MainActivity.this);
+        dbh.close();
+    }
+}
